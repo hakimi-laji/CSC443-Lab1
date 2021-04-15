ee948b917a7694852edae3b366eb45ac9b1a8582
Hakimi Laji <72840941+hakimi-laji@users.noreply.github.com>
2021-04-15 15:29:34 +0800

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Resume: Hakimi Laji</title>
+</head>
+<body>
+
+<h1 align="left">Resume</h1>
+
+<table border="1">
+
+<tr>
+    <td rowspan="6"><img src="https://lh3.googleusercontent.com/AKioYcKV0rGv3vGjx_HUpdunfo4ImfZjLZD5Vz84ueB3X7PgTYE5uM6nlCp9LqB2Hr4Z4dXssBw-PRMdXdgOrIzSgiifIjn84rgYhTU_3x4QORfln4izDmMTkGl1pDg7P6DYUAr428BXRIbiknRyUqb39OaMVl5m4sAYJcpSiaKIqLbtnSUFH6sdWFhOaEJa3RZVh_Ly0Ni4fqJb2tzjR8WctkRljoBRfLpzN13Ph9p6YDCrF6lTGhzjW-zv5QUy1TwaONbIum9vdjWqzyvK3Gv0a8D1AK3FUbayaVZcWzi8p1_uN_4ReX2LBT8IfC2k3skqzk8KO7gdui0fx4GFrEbTlK-Yc4sgMSUhXOVOWjlYyy6AL2sd0t-Xvq4sUJpeiNWV9cWnUKMp1LTE0Tw5vzGvQ0K-GGUTpwB6cu0O8OnhCipBgTABmBgpHDXVZ5QjRUot_klQu7Gy1mrNxUIxLbN-dnG2KEUWJg347lPsZ-WqkXcYyBNrORkpxbb1cQj8JCCuOnxkoQOYDuI9zLMekbr4FmC-AciHv2Y6u_ebYFIQAKoxqxuZMkKsLSBa-Pzy3qjJgjTl57PflgipQRgv0WgVqaZiweahTIb_Cjya6m2URVM1JOKTJU9XNJSVy2exeEioX5rKHx2r0D0c0hm-7GU0oXNrrbPmyan3xxnP3qU74StjkNjRMjPvgkFdhoKD0FntXVxxE9PsOb_vsBAe1Z19ZQ=w655-h873-no?authuser=0" style="width:250px;height:200px;"></td>
+    <th colspan="3">Profile</th>
+</tr>
+<tr>
+    <th>Name</th>
+    <td>Muhammad Hakimi bin Mohd Laji</td>
+</tr>
+<tr>
+    <th>Age</th>
+    <td>23</td>
+</tr>
+<tr>
+    <th>Faculty</th>
+    <td>Faculty of Computer Science and Mathematics</td>
+</tr>
+<tr>
+    <th>Program</th>
+    <td>Data Communication and Software Engineering</td>
+</tr>
+<tr>
+    <th>Email</th>
+    <td>hakimilaji@gmail.com</td>
+</tr>
+<tr>
+    <th colspan="3">Education Background</th>
+</tr>
+<tr>
+    <td><b>Duration</b></td>
+    <td colspan="2"><b>School</b></td>
+</tr>
+<tr>
+    <td>2019-Present</td>
+    <td colspan="2">Universiti Teknologi Mara</td>
+</tr>
+<tr>
+    <td>2016-2018</td>
+    <td colspan="2">Universiti Malaysia Pahang</td>
+</tr>
+<tr>
+    <td>2011-2015</td>
+    <td colspan="2">SMKA Durian Guling</td>
+</tr>
+
+<tr>
+    <th colspan="3">Work Experiences</th>
+</tr>
+<tr>
+    <td><b>Year</b></td>
+    <td colspan="2"><b>Job</b></td>
+</tr>
+<tr>
+    <td>2019</td>
+    <td colspan="2">Photocopier and networking technician at Fuji Xerox</td>
+</tr>
+<tr>
+    <td>2017-2018</td>
+    <td colspan="2">PC technician and sales rep at Aidea.com Technology</td>
+</tr>
+
+
+<tr>
+    <th colspan="3">Achievement</th>
+</tr>
+<tr>
+    <td><b>Year</b></td>
+    <td colspan="2"><b>Award</b></td>
+</tr>
+<tr>
+    <td>2017</td>
+    <td colspan="2">UMP Hack-a-thon 2017</td>
+</tr>
+<tr>
+    <td>2014, 2015</td>
+    <td colspan="2">Sultan Mahmud Bridge International Run 2014 and 2015</td>
+</tr>
+<tr>
+    <td>2014</td>
+    <td colspan="2">Kangaroo International Math Competition 2014</td>
+</tr>
+
+
+<tr>
+    <th colspan="3">Life Motto</th>
+</tr>
+<tr>
+	<td colspan="3">
+	<ul>
+    <i><center>Susah dahulu, senang kemudian</center></i>
+    </ul>
+	</td>
+</tr>
+
+<tr>
+    <th colspan="3">Interests</th>
+</tr>
+<tr>
+	<td colspan="3">
+	<ul>
+    <li>Learning to code and programming</li>
+    <li>Enjoying sci-fi movies and books</li>
+    <li>Watching questionable chinese cartoons</li>
+	<li>Browsing mongolian basket weaving forum</li>
+	<li>Chatting with like-minded strangers</li>
+    </ul>
+	</td>
+</tr>
+<tr>
+    <th colspan="3">Contact me</th>
+</tr>
+<tr>
+	<td colspan="3">
+	<ul>
+    <li><a href="https://www.facebook.com/hakimi.laji.98/">Facebook</a></li>
+    <li><a href="https://github.com/hakimi-laji">GitHub</a></li>
+	<li><a href="https://www.linkedin.com/in/hakimi-laji/">LinkedIn</a></li>
+    </ul>
+	</td>
+</tr>
+
+</table>
+
+</body>
+</html>
